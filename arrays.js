ffe6851d373b23f89487af1eed0cbd23706f0b5e
Julie Yego <julieyego@strathmore.edu>
2024-07-15 08:47:51 +0300

    array methods

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ console.log(friends);
 
 const years = new Array(2001, 2003, 1998);
 console.log('Years', years);
+years.push(2024);
+console.log(years, 'push');
+console.log(years.unshift(1990), 'unshift');
