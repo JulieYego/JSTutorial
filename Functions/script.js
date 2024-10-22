9c5ee8e0f39fb9ce47d61202abe8f27ff8b33ff8
JulieYego <yegojulie19@gmail.com>
2024-10-22 12:28:03 +0300

    call and apply start

--- Functions/script.js
+++ Functions/script.js
@@ -1,110 +1,125 @@
-// // 'use strict';
-// // const bookings = [];
+// // // 'use strict';
+// // // const bookings = [];
 
-// // const createBooking = function (
-// //   flightNum,
-// //   numPassengers = 1,
-// //   price = 199 * numPassengers
-// // ) {
-// //   //   numPassengers = numPassengers || 1;
-// //   //   price = price || 199;
-// //   const booking = {
-// //     flightNum,
-// //     numPassengers,
-// //     price,
-// //   };
-// //   console.log(booking);
-// //   bookings.push(booking);
+// // // const createBooking = function (
+// // //   flightNum,
+// // //   numPassengers = 1,
+// // //   price = 199 * numPassengers
+// // // ) {
+// // //   //   numPassengers = numPassengers || 1;
+// // //   //   price = price || 199;
+// // //   const booking = {
+// // //     flightNum,
+// // //     numPassengers,
+// // //     price,
+// // //   };
+// // //   console.log(booking);
+// // //   bookings.push(booking);
+// // // };
+
+// // // createBooking('LH44'); // LH44,1,199
+// // // createBooking('LH44', 2, 800); // LH44,2,800
+// // // createBooking('LH44', 2); // LH44, 2, 398
+// // // createBooking('LH44', 5); // LH44, 5, 995
+// // // createBooking('LH44', undefined, 1000); // LH44,1,1000
+
+// // // console.log(bookings);
+
+// // // // function sayHello(fName = 'Malia') {
+// // // //   console.log(`Hello ${fName}`);
+// // // // }
+
+// // // // sayHello(undefined);
+// // // // sayHello(null);
+// // // // sayHello();
+// // // // sayHello('Lydia');
+
+// // const flight = 'LH44';
+// // const lewis = {
+// //   name: 'Lewis Hamilton',
+// //   passport: 123456789,
 // // };
 
-// // createBooking('LH44'); // LH44,1,199
-// // createBooking('LH44', 2, 800); // LH44,2,800
-// // createBooking('LH44', 2); // LH44, 2, 398
-// // createBooking('LH44', 5); // LH44, 5, 995
-// // createBooking('LH44', undefined, 1000); // LH44,1,1000
+// // const checkIn = function (flightNum, passenger) {
+// //   flightNum = 'LH01';
+// //   passenger.name = 'Mr. ' + passenger.name;
 
-// // console.log(bookings);
+// //   if (passenger.passport === 123456789) {
+// //     alert('Checked In');
+// //   } else {
+// //     alert('wrong passport');
+// //   }
+// // };
 
-// // // function sayHello(fName = 'Malia') {
-// // //   console.log(`Hello ${fName}`);
-// // // }
+// // // checkIn(flight, lewis);
+// // // console.log(flight); // LH44
+// // // console.log(lewis);
 
-// // // sayHello(undefined);
-// // // sayHello(null);
-// // // sayHello();
-// // // sayHello('Lydia');
+// // const newPassport = function (person) {
+// //   person.passport = Math.trunc(Math.random() * 10000000000);
+// // };
 
-// const flight = 'LH44';
-// const lewis = {
-//   name: 'Lewis Hamilton',
-//   passport: 123456789,
-// };
-
-// const checkIn = function (flightNum, passenger) {
-//   flightNum = 'LH01';
-//   passenger.name = 'Mr. ' + passenger.name;
-
-//   if (passenger.passport === 123456789) {
-//     alert('Checked In');
-//   } else {
-//     alert('wrong passport');
-//   }
-// };
-
+// // newPassport(lewis);
 // // checkIn(flight, lewis);
-// // console.log(flight); // LH44
-// // console.log(lewis);
 
-// const newPassport = function (person) {
-//   person.passport = Math.trunc(Math.random() * 10000000000);
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
 // };
 
-// newPassport(lewis);
-// checkIn(flight, lewis);
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+// // Higher order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// // transformer('JavaScript is the best!', upperFirstWord);
+// // transformer('JavaScript is the best!', oneWord);
+
+// const high5 = function () {
+//   console.log('👋🏾');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// // ['Kevin', 'Joe', 'Nick', 'Frankie'].forEach(high5);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// const sayHey = greet('Hey');
+// const sayBye = greet('Bye');
+
+// sayHey('Jonas');
+// sayHey('Stewart');
+
+// sayBye('Aria');
+
+// greet('Hola')('Carlos');
+// greetArrow('Hola')('Carlota');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function () {},
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+  },
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
-
-// Higher order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-
-  console.log(`Transformed by: ${fn.name}`);
-};
-
-// transformer('JavaScript is the best!', upperFirstWord);
-// transformer('JavaScript is the best!', oneWord);
-
-const high5 = function () {
-  console.log('👋🏾');
-};
-
-document.body.addEventListener('click', high5);
-
-// ['Kevin', 'Joe', 'Nick', 'Frankie'].forEach(high5);
-
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
-
-const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
-
-const sayHey = greet('Hey');
-const sayBye = greet('Bye');
-
-sayHey('Jonas');
-sayHey('Stewart');
-
-sayBye('Aria');
-
-greet('Hola')('Carlos');
-greetArrow('Hola')('Carlota');
+lufthansa.book(239, 'Jonas');
+lufthansa.book(635, 'John');
