b0edcdb042fc5d9c72208713142c7864bc0e0a50
Julie Yego <julieyego@strathmore.edu>
2024-10-23 23:05:20 +0300

    iife

--- Functions/script.js
+++ Functions/script.js
@@ -1,248 +1,261 @@
-'use strict';
-// // // const bookings = [];
+// 'use strict';
+// // // // const bookings = [];
 
-// // // const createBooking = function (
-// // //   flightNum,
-// // //   numPassengers = 1,
-// // //   price = 199 * numPassengers
-// // // ) {
-// // //   //   numPassengers = numPassengers || 1;
-// // //   //   price = price || 199;
-// // //   const booking = {
-// // //     flightNum,
-// // //     numPassengers,
-// // //     price,
-// // //   };
-// // //   console.log(booking);
-// // //   bookings.push(booking);
+// // // // const createBooking = function (
+// // // //   flightNum,
+// // // //   numPassengers = 1,
+// // // //   price = 199 * numPassengers
+// // // // ) {
+// // // //   //   numPassengers = numPassengers || 1;
+// // // //   //   price = price || 199;
+// // // //   const booking = {
+// // // //     flightNum,
+// // // //     numPassengers,
+// // // //     price,
+// // // //   };
+// // // //   console.log(booking);
+// // // //   bookings.push(booking);
+// // // // };
+
+// // // // createBooking('LH44'); // LH44,1,199
+// // // // createBooking('LH44', 2, 800); // LH44,2,800
+// // // // createBooking('LH44', 2); // LH44, 2, 398
+// // // // createBooking('LH44', 5); // LH44, 5, 995
+// // // // createBooking('LH44', undefined, 1000); // LH44,1,1000
+
+// // // // console.log(bookings);
+
+// // // // // function sayHello(fName = 'Malia') {
+// // // // //   console.log(`Hello ${fName}`);
+// // // // // }
+
+// // // // // sayHello(undefined);
+// // // // // sayHello(null);
+// // // // // sayHello();
+// // // // // sayHello('Lydia');
+
+// // // const flight = 'LH44';
+// // // const lewis = {
+// // //   name: 'Lewis Hamilton',
+// // //   passport: 123456789,
 // // // };
 
-// // // createBooking('LH44'); // LH44,1,199
-// // // createBooking('LH44', 2, 800); // LH44,2,800
-// // // createBooking('LH44', 2); // LH44, 2, 398
-// // // createBooking('LH44', 5); // LH44, 5, 995
-// // // createBooking('LH44', undefined, 1000); // LH44,1,1000
+// // // const checkIn = function (flightNum, passenger) {
+// // //   flightNum = 'LH01';
+// // //   passenger.name = 'Mr. ' + passenger.name;
 
-// // // console.log(bookings);
+// // //   if (passenger.passport === 123456789) {
+// // //     alert('Checked In');
+// // //   } else {
+// // //     alert('wrong passport');
+// // //   }
+// // // };
 
-// // // // function sayHello(fName = 'Malia') {
-// // // //   console.log(`Hello ${fName}`);
-// // // // }
+// // // // checkIn(flight, lewis);
+// // // // console.log(flight); // LH44
+// // // // console.log(lewis);
 
-// // // // sayHello(undefined);
-// // // // sayHello(null);
-// // // // sayHello();
-// // // // sayHello('Lydia');
+// // // const newPassport = function (person) {
+// // //   person.passport = Math.trunc(Math.random() * 10000000000);
+// // // };
 
-// // const flight = 'LH44';
-// // const lewis = {
-// //   name: 'Lewis Hamilton',
-// //   passport: 123456789,
-// // };
-
-// // const checkIn = function (flightNum, passenger) {
-// //   flightNum = 'LH01';
-// //   passenger.name = 'Mr. ' + passenger.name;
-
-// //   if (passenger.passport === 123456789) {
-// //     alert('Checked In');
-// //   } else {
-// //     alert('wrong passport');
-// //   }
-// // };
-
+// // // newPassport(lewis);
 // // // checkIn(flight, lewis);
-// // // console.log(flight); // LH44
-// // // console.log(lewis);
 
-// // const newPassport = function (person) {
-// //   person.passport = Math.trunc(Math.random() * 10000000000);
+// // const oneWord = function (str) {
+// //   return str.replace(/ /g, '').toLowerCase();
 // // };
 
-// // newPassport(lewis);
-// // checkIn(flight, lewis);
+// // const upperFirstWord = function (str) {
+// //   const [first, ...others] = str.split(' ');
+// //   return [first.toUpperCase(), ...others].join(' ');
+// // };
 
-// const oneWord = function (str) {
-//   return str.replace(/ /g, '').toLowerCase();
+// // // Higher order function
+// // const transformer = function (str, fn) {
+// //   console.log(`Original string: ${str}`);
+// //   console.log(`Transformed string: ${fn(str)}`);
+
+// //   console.log(`Transformed by: ${fn.name}`);
+// // };
+
+// // // transformer('JavaScript is the best!', upperFirstWord);
+// // // transformer('JavaScript is the best!', oneWord);
+
+// // const high5 = function () {
+// //   console.log('👋🏾');
+// // };
+
+// // document.body.addEventListener('click', high5);
+
+// // // ['Kevin', 'Joe', 'Nick', 'Frankie'].forEach(high5);
+
+// // const greet = function (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting} ${name}`);
+// //   };
+// // };
+
+// // const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// // const sayHey = greet('Hey');
+// // const sayBye = greet('Bye');
+
+// // sayHey('Jonas');
+// // sayHey('Stewart');
+
+// // sayBye('Aria');
+
+// // greet('Hola')('Carlos');
+// // greetArrow('Hola')('Carlota');
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   // book: function () {},
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
 // };
 
-// const upperFirstWord = function (str) {
-//   const [first, ...others] = str.split(' ');
-//   return [first.toUpperCase(), ...others].join(' ');
+// lufthansa.book(239, 'Jonas');
+// lufthansa.book(635, 'John');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
 // };
 
-// // Higher order function
-// const transformer = function (str, fn) {
-//   console.log(`Original string: ${str}`);
-//   console.log(`Transformed string: ${fn(str)}`);
+// const book = lufthansa.book; // Method borrowing where we store the method in a variable then can be used as a regular function in elsewhere
 
-//   console.log(`Transformed by: ${fn.name}`);
+// // book(23, 'Sarah Williams'); // Uncaught TypeError: Cannot read property 'airline' of undefined because 'this' keyword is undefined since book is now a regular function call not a method call
+
+// // Call method
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
+
+// book.call(lufthansa, 239, 'DB Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
 // };
 
-// // transformer('JavaScript is the best!', upperFirstWord);
-// // transformer('JavaScript is the best!', oneWord);
+// book.call(swiss, 583, 'Cooper Barnes');
+// console.log(swiss);
 
-// const high5 = function () {
-//   console.log('👋🏾');
+// // bind method
+// const bookEW = book.bind(eurowings); // a new function created with the 'this' keyword set to eurowings
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+// bookEW(23, 'Steven Williams');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Martha Cooper');
+
+// // function myFunction() {
+// //   console.log(this);
+// // }
+
+// // // myFunction();
+
+// // const obj = {
+// //   someKey: 1,
+// //   myFunction: myFunction,
+// // };
+
+// // obj.myFunction();
+// // const newFunction = obj.myFunction;
+// // newFunction(); // Window {}
+// // newFunction.call(obj);
+
+// const mercedes = {
+//   team: 'Mercedes',
+//   based: 'UK',
+//   drivers: [],
+//   bookDriver(driverNum, name) {
+//     console.log(`${name} ${driverNum} booked a seat on ${this.team}`);
+//     this.drivers.push({ name, driverNum });
+//   },
 // };
 
-// document.body.addEventListener('click', high5);
+// mercedes.bookDriver(44, 'Lewis Hamilton');
 
-// // ['Kevin', 'Joe', 'Nick', 'Frankie'].forEach(high5);
+// const ferrari = {
+//   team: 'Ferrari',
+//   based: 'Italy',
+//   drivers: [],
+// };
+// const bookDriver = mercedes.bookDriver;
 
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
+// // bookDriver(16, 'Charles Leclerc');
+// bookDriver.call(ferrari, 16, 'Charles Leclerc');
+
+// // mercedes.book(44, 'Lewis Hamilton');
+// // console.log(mercedes);
+// // mercedes.book(77, 'Valtteri Bottas');
+
+// // Apply method
+// bookDriver.apply(ferrari, [5, 'Sebastian Vettel']);
+
+// // bind
+// const bookFerrari = bookDriver.bind(ferrari);
+// const bookFerrari99 = bookDriver.bind(ferrari, 99);
+// bookFerrari(99, 'Michael Schumacher');
+// bookFerrari99('Mick Schumacher');
+
+// mercedes.bookDriver(44, 'Lewis Hamilton');
+
+// // With event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// // lufthansa.buyPlane();
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value * 0.23;
+
+// console.log(addVAT(100));
+
+// const addTax2 = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
 //   };
 // };
 
-// const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+// const addVAT2 = addTax2(0.23);
+// console.log(addVAT2(100));
 
-// const sayHey = greet('Hey');
-// const sayBye = greet('Bye');
-
-// sayHey('Jonas');
-// sayHey('Stewart');
-
-// sayBye('Aria');
-
-// greet('Hola')('Carlos');
-// greetArrow('Hola')('Carlota');
-
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  // book: function () {},
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+// IIFE
+const runOnce = function () {
+  console.log('This will never run again');
+  const isPrivate = 23; // this is encapsulated inside the runOnce function scope
 };
 
-lufthansa.book(239, 'Jonas');
-lufthansa.book(635, 'John');
-console.log(lufthansa);
+(function () {
+  console.log('This will never run again');
+})();
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-const book = lufthansa.book; // Method borrowing where we store the method in a variable then can be used as a regular function in elsewhere
-
-// book(23, 'Sarah Williams'); // Uncaught TypeError: Cannot read property 'airline' of undefined because 'this' keyword is undefined since book is now a regular function call not a method call
-
-// Call method
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
-
-book.call(lufthansa, 239, 'DB Cooper');
-console.log(lufthansa);
-
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-
-book.call(swiss, 583, 'Cooper Barnes');
-console.log(swiss);
-
-// bind method
-const bookEW = book.bind(eurowings); // a new function created with the 'this' keyword set to eurowings
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-bookEW(23, 'Steven Williams');
-
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Martha Cooper');
-
-// function myFunction() {
-//   console.log(this);
-// }
-
-// // myFunction();
-
-// const obj = {
-//   someKey: 1,
-//   myFunction: myFunction,
-// };
-
-// obj.myFunction();
-// const newFunction = obj.myFunction;
-// newFunction(); // Window {}
-// newFunction.call(obj);
-
-const mercedes = {
-  team: 'Mercedes',
-  based: 'UK',
-  drivers: [],
-  bookDriver(driverNum, name) {
-    console.log(`${name} ${driverNum} booked a seat on ${this.team}`);
-    this.drivers.push({ name, driverNum });
-  },
-};
-
-mercedes.bookDriver(44, 'Lewis Hamilton');
-
-const ferrari = {
-  team: 'Ferrari',
-  based: 'Italy',
-  drivers: [],
-};
-const bookDriver = mercedes.bookDriver;
-
-// bookDriver(16, 'Charles Leclerc');
-bookDriver.call(ferrari, 16, 'Charles Leclerc');
-
-// mercedes.book(44, 'Lewis Hamilton');
-// console.log(mercedes);
-// mercedes.book(77, 'Valtteri Bottas');
-
-// Apply method
-bookDriver.apply(ferrari, [5, 'Sebastian Vettel']);
-
-// bind
-const bookFerrari = bookDriver.bind(ferrari);
-const bookFerrari99 = bookDriver.bind(ferrari, 99);
-bookFerrari(99, 'Michael Schumacher');
-bookFerrari99('Mick Schumacher');
-
-mercedes.bookDriver(44, 'Lewis Hamilton');
-
-// With event listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-
-  this.planes++;
-  console.log(this.planes);
-};
-
-// lufthansa.buyPlane();
-
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-// Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(null, 0.23);
-// addVAT = value => value + value * 0.23;
-
-console.log(addVAT(100));
-
-const addTax2 = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-
-const addVAT2 = addTax2(0.23);
-console.log(addVAT2(100));
+(() => console.log('This will also never run again'))();
+// console.log(isPrivate);
