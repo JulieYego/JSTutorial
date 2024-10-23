3ab62234a9d2e3e40e71ad5951cb4cb3d8b31d64
JulieYego <yegojulie19@gmail.com>
2024-10-23 16:51:24 +0300

    bind

--- Functions/script.js
+++ Functions/script.js
@@ -1,4 +1,4 @@
-// // // 'use strict';
+'use strict';
 // // // const bookings = [];
 
 // // // const createBooking = function (
@@ -118,8 +118,131 @@ const lufthansa = {
     console.log(
       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
     );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
   },
 };
 
 lufthansa.book(239, 'Jonas');
 lufthansa.book(635, 'John');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book; // Method borrowing where we store the method in a variable then can be used as a regular function in elsewhere
+
+// book(23, 'Sarah Williams'); // Uncaught TypeError: Cannot read property 'airline' of undefined because 'this' keyword is undefined since book is now a regular function call not a method call
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'DB Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Cooper Barnes');
+console.log(swiss);
+
+// bind method
+const bookEW = book.bind(eurowings); // a new function created with the 'this' keyword set to eurowings
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Martha Cooper');
+
+// function myFunction() {
+//   console.log(this);
+// }
+
+// // myFunction();
+
+// const obj = {
+//   someKey: 1,
+//   myFunction: myFunction,
+// };
+
+// obj.myFunction();
+// const newFunction = obj.myFunction;
+// newFunction(); // Window {}
+// newFunction.call(obj);
+
+const mercedes = {
+  team: 'Mercedes',
+  based: 'UK',
+  drivers: [],
+  bookDriver(driverNum, name) {
+    console.log(`${name} ${driverNum} booked a seat on ${this.team}`);
+    this.drivers.push({ name, driverNum });
+  },
+};
+
+mercedes.bookDriver(44, 'Lewis Hamilton');
+
+const ferrari = {
+  team: 'Ferrari',
+  based: 'Italy',
+  drivers: [],
+};
+const bookDriver = mercedes.bookDriver;
+
+// bookDriver(16, 'Charles Leclerc');
+bookDriver.call(ferrari, 16, 'Charles Leclerc');
+
+// mercedes.book(44, 'Lewis Hamilton');
+// console.log(mercedes);
+// mercedes.book(77, 'Valtteri Bottas');
+
+// Apply method
+bookDriver.apply(ferrari, [5, 'Sebastian Vettel']);
+
+// bind
+const bookFerrari = bookDriver.bind(ferrari);
+const bookFerrari99 = bookDriver.bind(ferrari, 99);
+bookFerrari(99, 'Michael Schumacher');
+bookFerrari99('Mick Schumacher');
+
+mercedes.bookDriver(44, 'Lewis Hamilton');
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+
+const addTax2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTax2(0.23);
+console.log(addVAT2(100));
