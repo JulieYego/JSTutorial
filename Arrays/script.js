bce9c60a99bd4425991352446fb0085566f5d584
JulieYego <yegojulie19@gmail.com>
2024-11-05 22:33:18 +0300

    creating DOM elements

--- Arrays/script.js
+++ Arrays/script.js
@@ -61,47 +61,116 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (movement, index) {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      index + 1
+    } ${type}</div>
+      <div class="movements__value">${movement}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+// console.log(containerMovements.innerHTML);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 // SLICE METHOD
-let arr = ['a', 'b', 'c', 'd', 'e'];
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice()); //shallow copy
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice()); //shallow copy
 
-// SPLICE METHOD
-console.log(arr.splice(1, 2, 'f', 'g'));
-console.log(arr);
+// // SPLICE METHOD
+// console.log(arr.splice(1, 2, 'f', 'g'));
+// console.log(arr);
 
-// REVERSE METHOD
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
+// // REVERSE METHOD
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
 
-// CONCAT METHOD
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
+// // CONCAT METHOD
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
 
-// JOIN METHOD
-console.log(letters.join(' - '));
+// // JOIN METHOD
+// console.log(letters.join(' - '));
 
-// AT
-const arr3 = [23, 11, 64];
-console.log(arr3[0]);
-console.log(arr3.at(0));
+// // AT
+// const arr3 = [23, 11, 64];
+// console.log(arr3[0]);
+// console.log(arr3.at(0));
+// // getting the last element
+// console.log(arr3[arr3.length - 1]);
+// console.log(arr3.slice(-1)[0]);
+// console.log(arr3.at(-1));
+
+// // LOOPING ARRAYS: forEach
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // for (const movement of movements) {
+// for (const [index, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1} : You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+// console.log('---------------------------------');
+// movements.forEach(function (movement, index, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1} : You withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
+// // forEach with Maps and Sets
+// // Map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+// console.log(currencies);
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+// // Set
+// const currenciesUnique = new Set(['USD', 'EUR', 'USD', 'GBP', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, key, set) {
+//   console.log(`${value}: ${value}`);
+// });
+
+// const myArray = [1, 2, 3, 4, 5];
+// console.log(myArray);
+// console.log(`-----LOOPING THROUGH ARRAY-----`);
+// console.log(`-----FOR LOOP-----`);
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+// console.log(`-----FOR OF LOOP-----`);
+// for (const num of myArray) {
+//   console.log(num);
+// }
+// console.log(`-----FOR EACH METHOD-----`);
+// myArray.forEach(function (num) {
+//   console.log(num);
+// });
